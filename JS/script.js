13f633ed59e51b95fe4ee07b5cc94e958a4eec65
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-29 22:53:16 +0000

    create a function for each cuisine

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 function getTasty15() {
-    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_15_minutes';
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=under_15_minutes';
     const options = {
         method: 'GET',
         headers: {
@@ -12,14 +12,15 @@ function getTasty15() {
         .then(function(response) {
             return response.json();
         }).then(function(data) {
-            console.log(data.results);
+            console.log("15-minute recipes", data.results);
         });
 }
 
 getTasty15();
 
+
 function getTasty30() {
-    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=under_30_minutes';
     const options = {
         method: 'GET',
         headers: {
@@ -32,14 +33,15 @@ function getTasty30() {
         .then(function(response) {
             return response.json();
         }).then(function(data) {
-            console.log(data.results);
+            console.log("30-minute recipes", data.results);
         });
 }
 
 getTasty30();
 
+
 function getTasty45() {
-    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_45_minutes';
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=under_45_minutes';
     const options = {
         method: 'GET',
         headers: {
@@ -52,11 +54,116 @@ function getTasty45() {
         .then(function(response) {
             return response.json();
         }).then(function(data) {
-            console.log(data.results);
+            console.log("45-minute recipes", data.results);
         });
 }
 
 getTasty45();
+
+
+function getMexicanFood() {
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=mexican';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c7965e32a1mshbe160fd7fa536fap1de1ccjsn06ed08451b4a',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    return fetch(tastyURL, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log("Mexican recipes", data.results);
+        });
+}
+
+getMexicanFood();
+
+function getItalianFood() {
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=italian';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c7965e32a1mshbe160fd7fa536fap1de1ccjsn06ed08451b4a',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    return fetch(tastyURL, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log("Italian recipes", data.results);
+        });
+}
+
+getItalianFood();
+
+
+function getMediterraneanFood() {
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=mediterranean';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c7965e32a1mshbe160fd7fa536fap1de1ccjsn06ed08451b4a',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    return fetch(tastyURL, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log("Mediterranean recipes", data.results);
+        });
+}
+
+getMediterraneanFood();
+
+
+function getAsianFood() {
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=asian';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c7965e32a1mshbe160fd7fa536fap1de1ccjsn06ed08451b4a',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    return fetch(tastyURL, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log("Asian recipes", data.results);
+        });
+}
+
+getAsianFood();
+
+
+function getBritishFood() {
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=10&tags=british';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c7965e32a1mshbe160fd7fa536fap1de1ccjsn06ed08451b4a',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    return fetch(tastyURL, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log("British recipes", data.results);
+        });
+}
+
+getBritishFood();
+
 
 function convertAmount() {
     var ingredient = "sugar";
