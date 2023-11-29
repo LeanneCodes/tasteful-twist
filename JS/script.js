10de1e4db369ec8a8c4393807384f0898b965af5
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-29 21:40:51 +0000

    get two recipe apis working together

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,45 @@
-var subAPI = "8b1cea18610f4b56a5703c3300b944fe";
+function getTastyData() {
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c7965e32a1mshbe160fd7fa536fap1de1ccjsn06ed08451b4a',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
 
-console.log("https://api.spoonacular.com/food/ingredients/substitutes?ingredientName=butter");
+    return fetch(tastyURL, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log(data.results[0].sections[0].components[1].ingredient.name)
+            return data.results[0].sections[0].components[1].ingredient.name;
+        });
+}
+
+function getSubData() {
+    getTastyData().then(function(ingredient) {
+        console.log(ingredient);
+        var subAPI = "8b1cea18610f4b56a5703c3300b944fe";
+        var subURL = `https://api.spoonacular.com/food/ingredients/substitutes?ingredientName=${ingredient}&apiKey=${subAPI}`;
+
+        fetch(subURL)
+            .then(function(response) {
+                return response.json();
+            }).then(function(data) {
+                console.log(data);
+                if (data.status === 'success') {
+                    console.log(data.substitutes);
+                }
+            });
+    });
+}
+
+getSubData();
+
+
+
+
+
+
+
