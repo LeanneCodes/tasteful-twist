1cb303269a2b4b1753b46637aac961cf03f93ab0
LeanneCodes <leannegoldsmith95@yahoo.com>
2023-11-29 22:19:12 +0000

    create 3 tasty api functions for different recipe lengths and a convert function from spoonacular

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,24 @@
-function getTastyData() {
+function getTasty15() {
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_15_minutes';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c7965e32a1mshbe160fd7fa536fap1de1ccjsn06ed08451b4a',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    return fetch(tastyURL, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log(data.results);
+        });
+}
+
+getTasty15();
+
+function getTasty30() {
     const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes';
     const options = {
         method: 'GET',
@@ -12,33 +32,52 @@ function getTastyData() {
         .then(function(response) {
             return response.json();
         }).then(function(data) {
-            console.log(data.results[0].sections[0].components[1].ingredient.name)
-            return data.results[0].sections[0].components[1].ingredient.name;
+            console.log(data.results);
         });
 }
 
-function getSubData() {
-    getTastyData().then(function(ingredient) {
-        console.log(ingredient);
-        var subAPI = "8b1cea18610f4b56a5703c3300b944fe";
-        var subURL = `https://api.spoonacular.com/food/ingredients/substitutes?ingredientName=${ingredient}&apiKey=${subAPI}`;
+getTasty30();
 
-        fetch(subURL)
-            .then(function(response) {
-                return response.json();
-            }).then(function(data) {
-                console.log(data);
-                if (data.status === 'success') {
-                    console.log(data.substitutes);
-                }
-            });
-    });
+function getTasty45() {
+    const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_45_minutes';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c7965e32a1mshbe160fd7fa536fap1de1ccjsn06ed08451b4a',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+        }
+    };
+
+    return fetch(tastyURL, options)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log(data.results);
+        });
 }
 
-getSubData();
+getTasty45();
+
+function convertAmount() {
+    var ingredient = "sugar";
+    var amount = 2.5;
+    var imperial = "cups";
+    var metric = 'grams';
+    var subAPI = "8b1cea18610f4b56a5703c3300b944fe";
+    var convertURL = `https://api.spoonacular.com/recipes/convert?ingredientName=${ingredient}&sourceAmount=${amount}&sourceUnit=${imperial}&targetUnit=${metric}&apiKey=${subAPI}`;
+
+    fetch(convertURL)
+        .then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            console.log(data);
+        });
+}
+
+convertAmount();
 
 
-
+// console.log(data.results[0].sections[0].components[0].measurements[1].quantity)
 
 
 
